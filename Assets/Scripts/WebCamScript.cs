0a64ef58ca521968f5bf4700e2f4dc10c0d2c38a
Frank Hancock <frankjwh@aol.com>
2017-04-07 22:38:50 -0400

    First Commit, add camera & tilt successfully

---
+++ Assets/Scripts/WebCamScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamScript : MonoBehaviour {
+
+	float viewTilt;
+	public Text userInput;
+	Vector3 zAxis;
+
+	// Use this for initialization
+	void Start () {
+		WebCamTexture webcamTexture = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float.TryParse (userInput.text, out viewTilt);
+		if (viewTilt is float) {
+			zAxis = new Vector3 (0, 0, viewTilt);
+			transform.eulerAngles = Camera.main.transform.eulerAngles + zAxis;
+		} else
+			Debug.Log ("Not a float");
+	}
+}
