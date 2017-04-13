21bf7a14efa58b710b55efe29a12ad474525b980
Frank Hancock <frankjwh@aol.com>
2017-04-13 16:04:57 -0400

    Black background for VR

--- Assets/Scripts/TiltUI.cs
+++ Assets/Scripts/TiltUI.cs
@@ -4,52 +4,65 @@ using UnityEngine;
 using UnityEngine.VR;
 using UnityEngine.UI;
 
-
 public class TiltUI : MonoBehaviour {
-
+	
+	Button btn;
 	Canvas canvasUI;
 
+	public GameObject vrViewer;
 	public Button uiButton;
-	Button btn;
 
 	// Use this for initialization
 	void Start () {
-		VRSettings.enabled = !VRSettings.enabled;
+		VRSettings.enabled = false;
 		canvasUI = GameObject.FindWithTag ("TiltCanvas").GetComponent<Canvas> ();
-
+		//canvasUI.enabled = false; //TODO comment this when toggle test is done
 		btn = uiButton.GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
 	}// Start()
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved && VRSettings.enabled)
-		{
+
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase == TouchPhase.Began && VRSettings.enabled == true) {
+				displayUI ();
+			}
+			//StartCoroutine (TogglePause ());
+		}
+
+		if(Input.GetMouseButtonDown (0)) {
 			displayUI ();
 		}
+
+		// tester for VR toggle without UI, works without problem
+		//disable canvasUI first and comment out above foreach
+//		foreach (Touch touch in Input.touches) {
+//			if (touch.phase == TouchPhase.Began) {
+//				displayUI ();
+//			}
+//			StartCoroutine (TogglePause ());
+//		}
 
 	}// Update()
 
 	// UI button listener
 	void TaskOnClick() {
-		Debug.Log ("You clicked the UI button!");
 		canvasUI.enabled = false;
 		VRSettings.enabled = true;
-		Debug.Log ("ending the call");
+		StartCoroutine (TogglePause());
 	}// TaskOnClick()
 
+	// displays the tilt UI menu when screen is in VR mode and user touches screen
+	// deactivates VR and enables Canvas in order to display menu
 	void displayUI() {
 		VRSettings.enabled = false; //disable VR for options
 		canvasUI.enabled = true;
 	}// displayUI()
 
-	// remove listener when UI is disabled to resume Update()
-	void Destroy() {
-		btn.onClick.RemoveListener (TaskOnClick); 
-	}
 
-	//add listener again to reenable previous functionality
-	void Create() {
-		btn.onClick.AddListener (TaskOnClick);
+	IEnumerator TogglePause() {
+		yield return new WaitForSeconds(2);
 	}
+		
 }
